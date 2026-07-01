6c49ea1cd09cbba200a61c57272dec634d292f73
lam-nt-825921 <23020096@vnu.edu.vn>
2026-07-01 17:12:08 +0700

    refactor resource tree

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,4 +1,4 @@
-import LandingPageContainer from '@/features/landing/containers/landing-page-container';
+import LandingPageContainer from '@/components/features/landing/containers/landing-page-container';
 
 export default function HomePage() {
   return <LandingPageContainer />;

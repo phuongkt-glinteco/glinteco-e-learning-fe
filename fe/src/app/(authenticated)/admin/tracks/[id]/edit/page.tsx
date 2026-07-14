614d30b0894d22ae048a2d9e8d8b62f6b48280e0
lam-nt-825921 <23020096@vnu.edu.vn>
2026-07-14 12:01:09 +0700

    feat(admin): unify track detail and edit management views

--- fe/src/app/(authenticated)/admin/tracks/[id]/edit/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AdminEditTrackClient } from '@/components/features/tracks/edit/AdminEditTrackClient';
-
-export default async function EditTrackPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return <AdminEditTrackClient trackId={id} />;
-}
